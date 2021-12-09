e4af00dbb71609648269a4ce0e1796f35c9f832e
Anam Shaikh <anam.shk@outlook.com>
2021-12-09 13:54:22 -0500

    adding the interviewer feataure in the app

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const InterviewerList = props.interviewers.map((interviewer) => {
+   return <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === value}
+        setInterviewer={() => onChange(interviewer.id)}
+      />
+    })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__list text--light"></h4>
+      <ul className="interviewers__list">{InterviewerList}</ul>
+    </section>
+  );
+};
