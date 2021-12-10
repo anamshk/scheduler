36edaa5ad61b4e17c0de782a477758044f1a1ff1
Anam Shaikh <anam.shk@outlook.com>
2021-12-10 14:18:34 -0500

    refactored the interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,14 +2,15 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
+
 export default function InterviewerList(props) {
   const InterviewerList = props.interviewers.map((interviewer) => {
    return <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === value}
-        setInterviewer={() => onChange(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     })
 
