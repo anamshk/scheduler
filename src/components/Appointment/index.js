6506c9cd9f92dae2ef44feb2378be29fdb458a87
Anam Shaikh <anam.shk@outlook.com>
2021-12-11 20:54:40 -0500

    updates on the appointment to have a show or empty section with the available interview appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.js";
+import Show from "components/Appointment/Show.js";
+import Empty from "components/Appointment/Empty.js";
+
+
 
 export default function Appointment(props) {
 
-  const getAppointmentString = (time) => {
-    if (time) {
-      return `Appointment at ${time}`
-    } else {
-      return `No Appointments`
-    }
-  }
+  // const getAppointmentString = (time) => {
+  //   if (time) {
+  //     return `Appointment at ${time}`
+  //   } else {
+  //     return `No Appointments`
+  //   }
+  // }
+
 
   return (
-    <article className="appointment">{getAppointmentString(props.time)}</article>
+    <>
+    <Header time={props.time}/> 
+      { props.interview ? <Show />: <Empty/>}
+      {/* <article className="appointment">{props.time}</article> */}
+    </>
+    
   )
 };
