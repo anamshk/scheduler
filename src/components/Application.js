fcfb8ec54d174d37bc51477d16f8beb811535453
Anam Shaikh <anam.shk@outlook.com>
2021-12-11 22:50:34 -0500

    implementing all the appointment updates made into the browser

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,46 @@ import React from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import { useState } from "react";
+import Appointment from "components/Appointment/index.js"
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
   const days = [
@@ -49,7 +89,18 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => {
+          return (
+          <Appointment
+            key={appointment.id}
+            {...appointment} 
+          />
+          )
+        })
+        }
+        
+        
+        
       </section>
     </main>
   );
