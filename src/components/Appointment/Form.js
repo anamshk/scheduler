b2e8a36b1bb89639b3ed484d31033652263e9887
Anam Shaikh <anam.shk@outlook.com>
2021-12-15 17:57:04 -0500

    updated files in accordance to reach protype testing

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,35 +1,52 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 
 export default function Form(props){
-  const [student, setStudent] = useState(props.student || "");
+  const [name, setName] = useState(props.name || "");
+
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const [error, setError] = useState("");
+
   const reset = (() => {
-    setStudent("")
+    setName("")
     setInterviewer("")
   });
-
+  
   const cancel = (() => {
     reset();
     props.onCancel();
   });
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
+
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
         type="text"
         placeholder="Enter Student Name"
-        value={student}
-        onChange={(event) => setStudent(event.target.value)}
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList 
       value={interviewer}
       interviewers={props.interviewers}
@@ -40,7 +57,7 @@ export default function Form(props){
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel} >Cancel</Button>
-      <Button confirm onClick={(event) => props.onSave(student, interviewer)}>Save</Button>
+      <Button confirm onClick={validate}>Save</Button>
     </section>
   </section>
 </main>
